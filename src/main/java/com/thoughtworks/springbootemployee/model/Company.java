0112cdadae7d0a8cc6511a994473cbcd12e4cb1e
Calvin Chu <calvinchu26@gmail.com>
2020-12-02 20:34:50 +0800

    [SpringBootPractice][Calvin] feat: added Company class

---
+++ src/main/java/com/thoughtworks/springbootemployee/model/Company.java
@@ -0,0 +1,42 @@
+package com.thoughtworks.springbootemployee.model;
+
+public class Company {
+    private String id;
+    private String name;
+    private Integer address;
+
+    public Company() {
+    }
+
+    public Company(String id, String name, Integer address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAddress() {
+        return address;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Company) {
+            return this.id.equals(((Company) obj).getId()) &&
+                    this.name.equals(((Company) obj).getName()) &&
+                    this.address.equals(((Company) obj).getAddress());
+        }
+        return super.equals(obj);
+    }
+}
