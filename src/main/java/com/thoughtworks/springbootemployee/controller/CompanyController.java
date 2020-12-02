7a22bad7442915664aae2d4daca90d7079476a38
Calvin Chu <calvinchu26@gmail.com>
2020-12-02 22:11:17 +0800

    [SpringBootPractice][Calvin] refactor: removed empty lines

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -17,7 +17,6 @@ public class CompanyController {
 
     @Autowired private CompanyService companyService;
 
-
     @GetMapping
     public List<Company> getAll() {
         return companyService.getAll();
