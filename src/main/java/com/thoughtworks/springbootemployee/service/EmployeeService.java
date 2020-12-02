9edd3587a35ed592db264245622644727505756e
Calvin Chu <calvinchu26@gmail.com>
2020-12-02 20:10:23 +0800

    [SpringBootPractice][Calvin] fix: specify @Service

--- src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
@@ -3,10 +3,12 @@ package com.thoughtworks.springbootemployee.service;
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class EmployeeService {
     @Autowired private EmployeeRepository employeeRepository;
 
