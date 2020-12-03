8bf024bff121deedf04942f6023cf01447ccaddf
Calvin Chu <calvinchu26@gmail.com>
2020-12-03 16:38:41 +0800

    [integration][Calvin] test: added teardown

--- src/test/java/com/thoughtworks/springbootemployee/integration/EmployeeIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/integration/EmployeeIntegrationTest.java
@@ -2,6 +2,7 @@ package com.thoughtworks.springbootemployee.integration;
 
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -20,6 +21,11 @@ class EmployeeIntegrationTest {
     private MockMvc mockMvc;
     @Autowired
     private EmployeeRepository employeeRepository;
+
+    @AfterEach
+    void tearDown() {
+        employeeRepository.deleteAll();
+    }
 
     @Test
     void should_return_all_employees_when_get_all_given_repository_has_employees() throws Exception {
