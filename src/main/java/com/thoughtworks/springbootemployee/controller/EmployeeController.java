cadfbf34d18193acc4fea203d8c8d23cb7c72172
Calvin Chu <calvinchu26@gmail.com>
2020-12-04 15:37:34 +0800

    [mapper][Calvin] update: employee with mapper

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -1,6 +1,9 @@
 package com.thoughtworks.springbootemployee.controller;
 
+import com.thoughtworks.springbootemployee.dto.EmployeeRequest;
+import com.thoughtworks.springbootemployee.dto.EmployeeResponse;
 import com.thoughtworks.springbootemployee.exception.EmployeeNotFoundException;
+import com.thoughtworks.springbootemployee.mapper.EmployeeMapper;
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,42 +11,45 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
 
     @Autowired private EmployeeService employeeService;
+    @Autowired private EmployeeMapper employeeMapper;
 
     @GetMapping
-    public List<Employee> getAll() {
-        return employeeService.getAll();
+    public List<EmployeeResponse> getAll() {
+        return employeeService.getAll().stream().map(employeeMapper::toResponse).collect(Collectors.toList());
     }
 
     @GetMapping("/{id}")
-    public Employee getOne(@PathVariable String id) throws EmployeeNotFoundException {
-        return employeeService.getOne(id);
+    public EmployeeResponse getOne(@PathVariable String id) throws EmployeeNotFoundException {
+        return employeeMapper.toResponse(employeeService.getOne(id));
     }
 
     @GetMapping(params = {"page", "pageSize"})
-    public List<Employee> getWithPaging(@RequestParam("page") Integer pageNumber, @RequestParam("pageSize") Integer pageSize) {
-        return employeeService.getWithPagination(pageNumber, pageSize);
+    public List<EmployeeResponse> getWithPaging(@RequestParam("page") Integer pageNumber, @RequestParam("pageSize") Integer pageSize) {
+        return employeeService.getWithPagination(pageNumber, pageSize).stream().map(employeeMapper::toResponse).collect(Collectors.toList());
     }
 
     @GetMapping(params = {"gender"})
-    public List<Employee> getWithGenderFilter(@RequestParam("gender") String gender) {
-        return employeeService.getWithGenderFilter(gender);
+    public List<EmployeeResponse> getWithGenderFilter(@RequestParam("gender") String gender) {
+        return employeeService.getWithGenderFilter(gender).stream().map(employeeMapper::toResponse).collect(Collectors.toList());
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Employee createNewEmployee(@RequestBody Employee newEmployee) {
-        return employeeService.create(newEmployee);
+    public EmployeeResponse createNewEmployee(@RequestBody EmployeeRequest employeeRequest) {
+        Employee employee = employeeService.create(employeeMapper.toEntity(employeeRequest));
+        return employeeMapper.toResponse(employee);
     }
 
     @PutMapping("/{id}")
-    public Employee updateEmployee(@PathVariable String id, @RequestBody Employee employeeUpdate) throws EmployeeNotFoundException {
-        return employeeService.update(id, employeeUpdate);
+    public EmployeeResponse updateEmployee(@PathVariable String id, @RequestBody EmployeeRequest employeeUpdate) throws EmployeeNotFoundException {
+        return employeeMapper.toResponse(employeeService.update(id, employeeMapper.toEntity(employeeUpdate)));
 
     }
 
