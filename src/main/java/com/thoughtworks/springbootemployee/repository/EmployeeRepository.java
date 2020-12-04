f56b6e33cd4758c849c83caf64373fccfd31ae31
Calvin Chu <calvinchu26@gmail.com>
2020-12-04 10:02:24 +0800

    [integration][Calvin] refactor: removed unused import

--- src/main/java/com/thoughtworks/springbootemployee/repository/EmployeeRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/repository/EmployeeRepository.java
@@ -4,7 +4,6 @@ import com.thoughtworks.springbootemployee.model.Employee;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface EmployeeRepository extends MongoRepository<Employee, String> {
     List<Employee> findAllByGender(String gender);
