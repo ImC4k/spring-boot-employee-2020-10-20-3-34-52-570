8ee888667d9b3fbf3f59446ba47087bcfd32c596
Calvin Chu <calvinchu26@gmail.com>
2020-12-05 09:31:58 +0800

    [api][Calvin] try out api management

---
+++ src/main/java/com/thoughtworks/springbootemployee/config/IncomingRequestInterceptor.java
@@ -0,0 +1,18 @@
+package com.thoughtworks.springbootemployee.config;
+
+import com.moesif.servlet.MoesifFilter;
+
+import javax.servlet.Filter;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.context.annotation.*;
+
+@Configuration
+public class IncomingRequestInterceptor extends WebMvcConfigurerAdapter {
+    @Value("${api.management.key}") String managementKey;
+    @Bean
+    public Filter moesifFilter() {
+        return new MoesifFilter(managementKey);
+    }
+}
